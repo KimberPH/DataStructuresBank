88521c283d0b224ba0a7a04761d987fab5dfdf1e
RichardJCase <johndoe27328@gmail.com>
2017-11-13 18:41:54 -0600

    main program outline

---
+++ Bank.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "Customer.h"
+
+class Bank {
+private:
+  std::vector<Customer> customers;
+  std::vector<Transaction> transactions;
+  
+public:
+  Bank();
+  //~Bank();
+
+  // TODO: add args when known
+  void addTransaction(double startBalance, const Date &date, const std::string &payee);
+  
+  bool loadCustomers(const char *path);
+  bool loadAccounts(const char *path);
+  bool loadTransactions(const char *path);
+
+  std::string showAccounts() const;
+  std::string monthlyStatement() const;
+  float totalSavings() const;
+  float totalChecking() const;
+  float totalCD() const;
+  std::string showCustomers() const;
+  bool save() const;
+};
---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <signal.h>
+#include <stdlib.h>
+#include <iostream>
+//#include "bittypes.h"
+//#include "Bank.h"
+
+using namespace std;
+
+static void signalHandler(int signo){
+  if(signo == SIGSEGV){
+    cout << "Nothing to see here. Everything's fine.\n";
+    exit(1);
+  }
+}
+
+static void printOptions(){
+  cout << "1. enter another transaction\n";
+  cout << "2. list all of a customer's accounts\n";
+  cout << "3. print a monthly statement for a given account\n";
+  cout << "4. print the total value of all of the savings accounts\n";
+  cout << "5. print the total value of all of the checking accounts\n";
+  cout << "6. print the total value of all of the CD accounts\n";
+  cout << "7. print a list of customers with the total value of all of their accounts\n";
+  cout << "8. save the current collection of customers, accounts and transactions to the appropriate files\n";
+
+  cout << "\nEnter option: ";
+}
+
+int main(int argc, char **argv){
+  signal(SIGSEGV, signalHandler);
+  
+  Bank bank;
+
+  if(argc > 3){
+    // TODO: read files
+  }else{
+    // TODO: ask for input to read files
+  }
+  
+  
+  u16 cmd;
+  while(true){
+    printOptions();
+    cin >> cmd;
+    switch(cmd){
+      // TODO: 
+    default:
+    }
+  }
+  
+  return 0;
+}
